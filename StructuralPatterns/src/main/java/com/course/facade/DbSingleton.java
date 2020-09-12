af6296118c745fc166facd4166b18d9c763eb5a9
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2020-09-12 14:51:13 -0300

    project with Facade example

---
+++ StructuralPatterns/src/main/java/com/course/facade/DbSingleton.java
@@ -0,0 +1,63 @@
+package com.course.facade;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbSingleton {
+	
+	private static volatile DbSingleton instance = null;
+	private static volatile Connection conn = null;
+	
+	private DbSingleton() {
+		
+		try {
+			Driver d = new org.apache.derby.jdbc.EmbeddedDriver();
+			
+			DriverManager.registerDriver(d);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}		
+		
+		if (conn != null)
+			throw new RuntimeException("Use getConnection() method to create");	
+		
+		if (instance != null)
+			throw new RuntimeException("Use getInstance() method to create");
+	}	
+	
+	public static DbSingleton getInstance() {
+		
+		if (instance == null) {
+			synchronized (DbSingleton.class) {
+				if (instance == null) {
+					instance = new DbSingleton();
+				
+				}
+			}
+			
+		}		
+		return instance;
+
+	}
+	
+	public Connection getConnection() {
+		
+		if (conn == null) {
+			synchronized (DbSingleton.class) {
+				if (conn == null) {
+					try {
+						String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+						conn = DriverManager.getConnection(dbUrl);
+						
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		}
+		return conn;		
+	}
+}
