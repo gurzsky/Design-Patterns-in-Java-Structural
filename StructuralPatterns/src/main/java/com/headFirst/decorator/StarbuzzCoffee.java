bb573a0d85d3d2058ee7c66e7e91d1579f715ac6
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2021-05-13 17:28:52 -0300

    reducing the boilerplate code

--- StructuralPatterns/src/main/java/com/headFirst/decorator/StarbuzzCoffee.java
+++ StructuralPatterns/src/main/java/com/headFirst/decorator/StarbuzzCoffee.java
@@ -6,24 +6,10 @@ public class StarbuzzCoffee {
         Beverage beverage = new Expresso();
         System.out.println(beverage.getDescription() + " $" + String.format("%.2f", beverage.cost()) + ", Size " + beverage.getSize());
 
-        Beverage beverage1 = new DarkRoast();
-        beverage1.setSize(Beverage.Size.GRANDE);
-        beverage1 = new Mocha(beverage1);
-        beverage1.setSize(Beverage.Size.GRANDE);
-        beverage1 = new Mocha(beverage1);
-        beverage1.setSize(Beverage.Size.GRANDE);
-        beverage1 = new Whip(beverage1);
-        beverage1.setSize(Beverage.Size.GRANDE);
+        Beverage beverage1 = new Whip(new Mocha(new Mocha(new DarkRoast())));
         System.out.println(beverage1.getDescription() + " $" + String.format("%.2f", beverage1.cost()) + ", Size " + beverage1.getSize());
 
-        Beverage beverage2 = new HouseBlend();
-        beverage2.setSize(Beverage.Size.TALL);
-        beverage2 = new Soy(beverage2);
-        beverage2.setSize(Beverage.Size.TALL);
-        beverage2 = new Mocha(beverage2);
-        beverage2.setSize(Beverage.Size.TALL);
-        beverage2 = new Whip(beverage2);
-        beverage2.setSize(Beverage.Size.TALL);
+        Beverage beverage2 = new Whip(new Mocha(new Soy(new HouseBlend())));
         System.out.println(beverage2.getDescription() + " $" + String.format("%.2f", beverage2.cost()) + ", Size " + beverage2.getSize());
     }
 }
